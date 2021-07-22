3e11c3fcd936aa70a9177eb0f61c015214182b79
hollisc <hollisc@ca.ibm.com>
2021-07-22 11:27:53 -0400

    Add pipeline sa yaml and update git cred secrets
    Signed-off-by: hollisc <hollisc@ca.ibm.com>

--- ace/environments/ci/secrets/gitops-cd-secret.sh
+++ ace/environments/ci/secrets/gitops-cd-secret.sh
@@ -6,11 +6,11 @@ GIT_TOKEN=<GIT-TOKEN>
 SEALEDSECRET_NAMESPACE=sealed-secrets
 
 # Create Kubernetes Secret yaml
-cat <<EOF > delete-git-source-read-creds-secret.yaml
+cat <<EOF > delete-gitops-cd-secret.yaml
 apiVersion: v1
 kind: Secret
 metadata:
-  name: git-source-read-creds
+  name: gitops-cd-secret
   annotations:
     tekton.dev/git-0: https://github.com 
 type: kubernetes.io/basic-auth
@@ -20,7 +20,7 @@ stringData:
 EOF
 
 # Encrypt the secret using kubeseal and private key from the cluster
-kubeseal --scope cluster-wide --controller-name=sealedsecretcontroller-sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-git-source-read-creds-secret.yaml > git-source-read-creds-secret.yaml
+kubeseal --scope cluster-wide --controller-name=sealedsecretcontroller-sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-gitops-cd-secret.yaml > gitops-cd-secret.yaml
 
 # NOTE, do not check delete-ibm-entitled-key-secret.yaml into git!
-rm delete-git-source-read-creds-secret.yaml
+rm delete-gitops-cd-secret.yaml
