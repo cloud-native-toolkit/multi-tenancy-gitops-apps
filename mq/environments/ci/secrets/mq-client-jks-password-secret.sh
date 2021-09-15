7a022ef72b2676182f3f3b5cc6aaa41c1829fc8e
hollisc <hollisc@ca.ibm.com>
2021-09-14 22:04:34 -0400

    Add script to generate and seal mq-client-jks-password
    Signed-off-by: hollisc <hollisc@ca.ibm.com>

---
+++ mq/environments/ci/secrets/mq-client-jks-password-secret.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Set variables
+if [[ -z ${KEY_STORE_PASSWORD} ]]; then
+  echo "Please provide environment variable KEY_STORE_PASSWORD"
+  exit 1
+fi
+
+SEALED_SECRET_NAMESPACE=${SEALED_SECRET_NAMESPACE:-sealed-secrets}
+SEALED_SECRET_CONTOLLER_NAME=${SEALED_SECRET_CONTOLLER_NAME:-sealed-secrets}
+
+# Create Kubernetes Secret yaml
+oc create secret generic mq-client-jks-password \
+--from-literal=KEY_STORE_PASSWORD=${KEY_STORE_PASSWORD} \
+--dry-run=client -o yaml > delete-mq-client-jks-password-secret.yaml
+
+# Encrypt the secret using kubeseal and private key from the cluster
+kubeseal --scope cluster-wide --controller-name=${SEALED_SECRET_CONTOLLER_NAME} --controller-namespace=${SEALED_SECRET_NAMESPACE} -o yaml < delete-mq-client-jks-password-secret.yaml > mq-client-jks-password-secret.yaml
+
+# NOTE, do not check delete-mq-client-jks-password-secret.yaml into git!
+rm delete-mq-client-jks-password-secret.yaml
