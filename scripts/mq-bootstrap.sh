bff5089c90af8570c29872ad0952b7fbd0384750
hollisc <hollisc@ca.ibm.com>
2021-09-17 14:09:23 -0400

    Update bootstrap.sh with calls to various scripts
    Signed-off-by: hollisc <hollisc@ca.ibm.com>

--- scripts/mq-bootstrap.sh
+++ scripts/mq-bootstrap.sh
@@ -47,6 +47,89 @@ mq_gitops_repo_cm () {
     popd    
 }
 
+kubseal_ibm_entitled_registry () {
+    pushd $ROOTDIR/mq/environments/ci/secrets
+    source ibm-entitled-registry-credentials-secret.sh
+    popd    
+}
+
+mq_kubseal_client_jks_pass_ci () {
+    pushd $ROOTDIR/mq/environments/ci/secrets
+    source mq-client-jks-password-secret.sh
+    popd    
+}
+
+mq_client_certificate_ci () {
+    pushd $ROOTDIR/mq/environments/ci/certificates
+    source ci-mq-client-certificate.sh
+    popd    
+}
+
+mq_server_certificate_ci () {
+    pushd $ROOTDIR/mq/environments/ci/certificates
+    source ci-mq-server-certificate.sh
+    popd    
+}
+
+mq_kubseal_client_jks_pass_dev () {
+    pushd $ROOTDIR/mq/environments/dev/secrets
+    source mq-client-jks-password-secret.sh
+    popd    
+}
+
+mq_client_certificate_dev () {
+    pushd $ROOTDIR/mq/environments/dev/certificates
+    source dev-mq-client-certificate.sh
+    popd    
+}
+
+mq_server_certificate_dev () {
+    pushd $ROOTDIR/mq/environments/dev/certificates
+    source dev-mq-server-certificate.sh
+    popd    
+}
+
+mq_kubseal_client_jks_pass_staging () {
+    pushd $ROOTDIR/mq/environments/staging/secrets
+    source mq-client-jks-password-secret.sh
+    popd    
+}
+
+mq_client_certificate_staging () {
+    pushd $ROOTDIR/mq/environments/staging/certificates
+    source staging-mq-client-certificate.sh
+    popd    
+}
+
+mq_server_certificate_staging () {
+    pushd $ROOTDIR/mq/environments/staging/certificates
+    source staging-mq-server-certificate.sh
+    popd    
+}
+
+mq_kubseal_client_jks_pass_prod () {
+    pushd $ROOTDIR/mq/environments/prod/secrets
+    source mq-client-jks-password-secret.sh
+    popd    
+}
+
+mq_client_certificate_prod () {
+    pushd $ROOTDIR/mq/environments/prod/certificates
+    source prod-mq-client-certificate.sh
+    popd    
+}
+
+mq_server_certificate_prod () {
+    pushd $ROOTDIR/mq/environments/prod/certificates
+    source prod-mq-server-certificate.sh
+    popd    
+
+mq_selfsigned_certificate () {
+    pushd $ROOTDIR/mq/environments/tools/certificates/mq-selfsigned-certificate
+    source mq-self-signed-ca-certificate.sh
+    popd    
+}
+
 mq_review_git () {
     pushd $ROOTDIR/mq
     git diff
@@ -62,17 +145,38 @@ mq_git_add_commit_push () {
 }
 
 # main
-
 wait_kubeseal_ready
 
+# execute scripts in mq/environments/ci 
 mq_kubeseal_artifactory
-
 mq_kubeseal_git
-
 mq_gitops_repo_cm
 
-mq_review_git
+# mq_kubseal_client_jks_pass_ci
+# mq_client_certificate_ci
+# mq_server_certificate_ci
+# kubseal_ibm_entitled_registry
 
+# execute scripts in mq/environments/dev
+# mq_kubseal_client_jks_pass_dev
+# mq_client_certificate_dev
+# mq_server_certificate_dev
+
+# execute scripts in mq/environments/staging
+# mq_kubseal_client_jks_pass_staging
+# mq_client_certificate_staging
+# mq_server_certificate_staging
+
+# execute scripts in mq/environments/prod
+# mq_kubseal_client_jks_pass_prod
+# mq_client_certificate_prod
+# mq_server_certificate_prod
+
+# execute scripts in mq/environments/tools
+#mq_selfsigned_certificate
+
+# review and commit
+mq_review_git
 mq_git_add_commit_push
 
 
